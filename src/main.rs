5f1f185ebbb9d0f1f88d4653d26d2da8d0484bcc
Ed Cashin <ed.cashin@acm.org>
2021-05-31 13:40:09 -0400

    Use None in work chan to indicate worker completion

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ const N_WORKERS: u32 = 10;
 
 fn do_work(worker_id: u32,
     paths_rx: chan::Receiver<path::PathBuf>,
-    wdescs_tx: chan::Sender<Option<WavDesc>>,
-    done_tx: chan::Sender<u32>) {
+    wdescs_tx: chan::Sender<Option<WavDesc>>) {
     for path in paths_rx {
         if path.is_file() {
             if let Some(ext) = path.extension() {
                 if let Some(ext) = ext.to_str() {
                     if ext.eq_ignore_ascii_case("wav") {
+                        println!("worker:{} sending for {:?}", worker_id, path);
                         wdescs_tx.send(Some(WavDesc {
                             path
                         }));
@@ -22,7 +22,6 @@ fn do_work(worker_id: u32,
         }
     }
     wdescs_tx.send(None);
-    done_tx.send(worker_id);
 }
 
 struct WavDesc {
@@ -34,17 +33,15 @@ struct WavDesc {
 fn main() {
     let dirs: Vec<String> = env::args().skip(1).collect();
 
-    let (done_tx, done_rx) = chan::sync(0);  // termination channel
     let (wdescs_tx, wdescs_rx) = chan::sync(0);  // wav description channel
     {
         // At the end of this scope, we're done sending directories.
         let (dirs_tx, dirs_rx) = chan::sync(0);
         for w in 0..N_WORKERS {
-            let done_tx = done_tx.clone();
             let dirs_rx = dirs_rx.clone();
             let wdescs_tx = wdescs_tx.clone();
             thread::spawn(move || {
-                do_work(w, dirs_rx, wdescs_tx, done_tx);
+                do_work(w, dirs_rx, wdescs_tx);
             });
         }
         for d in dirs {
@@ -55,27 +52,17 @@ fn main() {
         }
     }
     // Work consumer thread takes ownership of wdescs_rx.
-    {
-        let mut n_producers = N_WORKERS;   
-        let done_tx = done_tx.clone();
-        thread::spawn(move || {
-            while n_producers > 0 {
-                if let Some(wdesc_opt) = wdescs_rx.recv() {
-                    if let Some(wdesc) = wdesc_opt {
-                        println!("received {:?} with {} producers remaining", wdesc.path, n_producers);
-                    } else {
-                        println!("received None");
-                        n_producers -= 1;
-                    }
-                } else {
-                    panic!("producers don't close the work channel");
-                }
+    let mut n_producers = N_WORKERS;
+    while n_producers > 0 {
+        if let Some(wdesc_opt) = wdescs_rx.recv() {
+            if let Some(wdesc) = wdesc_opt {
+                println!("consumer received {:?} with {} producers remaining", wdesc.path, n_producers);
+            } else {
+                println!("consumer received None");
+                n_producers -= 1;
             }
-            done_tx.send(N_WORKERS);  // work consumer's ID is one greater than any producer's
-        });
-    }
-    for _ in 0..(N_WORKERS + 1) {
-        let wid = done_rx.recv();
-        println!("worker {:?} finished", wid);
+        } else {
+            panic!("producers don't close the work channel");
+        }
     }
 }
