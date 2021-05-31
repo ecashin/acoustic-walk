f92fa5e3e6095412e2ea3b51de331919bc7aac5c
Ed Cashin <ed.cashin@acm.org>
2021-05-31 13:11:14 -0400

    Add a channel for WAV descriptions

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,78 @@ use walkdir::WalkDir;
 
 const N_WORKERS: u32 = 10;
 
-fn do_work(worker_id: u32, rdirs: chan::Receiver<path::PathBuf>, done: chan::Sender<()>) {
-    for dir in rdirs {
-        println!("worker:{} dir:{:?}", worker_id, dir);
+fn do_work(worker_id: u32,
+    paths_rx: chan::Receiver<path::PathBuf>,
+    wdescs_tx: chan::Sender<Option<WavDesc>>,
+    done_tx: chan::Sender<u32>) {
+    for path in paths_rx {
+        if path.is_file() {
+            if let Some(ext) = path.extension() {
+                if let Some(ext) = ext.to_str() {
+                    if ext.eq_ignore_ascii_case("wav") {
+                        wdescs_tx.send(Some(WavDesc {
+                            path
+                        }));
+                    }
+                }
+            }
+        }
     }
-    done.send(());
+    wdescs_tx.send(None);
+    done_tx.send(worker_id);
+}
+
+struct WavDesc {
+    path: path::PathBuf,
+    // hdr: wav::Header,
+    // data: wav::BitDepth
 }
 
 fn main() {
     let dirs: Vec<String> = env::args().skip(1).collect();
 
-    let (sdone, rdone) = chan::sync(0);
+    let (done_tx, done_rx) = chan::sync(0);  // termination channel
+    let (wdescs_tx, wdescs_rx) = chan::sync(0);  // wav description channel
     {
         // At the end of this scope, we're done sending directories.
-        let (sdirs, rdirs) = chan::sync(0);
+        let (dirs_tx, dirs_rx) = chan::sync(0);
         for w in 0..N_WORKERS {
-            let sdone = sdone.clone();
-            let rdirs = rdirs.clone();
+            let done_tx = done_tx.clone();
+            let dirs_rx = dirs_rx.clone();
+            let wdescs_tx = wdescs_tx.clone();
             thread::spawn(move || {
-                do_work(w, rdirs, sdone);
+                do_work(w, dirs_rx, wdescs_tx, done_tx);
             });
         }
         for d in dirs {
             for entry in WalkDir::new(d).into_iter().filter_map(|e| e.ok()) {
                 let p = path::PathBuf::from(entry.path());
-                sdirs.send(p);
+                dirs_tx.send(p);
             }
         }
     }
-    for _ in 0..N_WORKERS {
-        rdone.recv();
+    // Work consumer thread takes ownership of wdescs_rx.
+    {
+        let mut n_producers = N_WORKERS;   
+        let done_tx = done_tx.clone();
+        thread::spawn(move || {
+            while n_producers > 0 {
+                if let Some(wdesc_opt) = wdescs_rx.recv() {
+                    if let Some(wdesc) = wdesc_opt {
+                        println!("received {:?} with {} producers remaining", wdesc.path, n_producers);
+                    } else {
+                        println!("received None");
+                        n_producers -= 1;
+                    }
+                } else {
+                    panic!("producers don't close the work channel");
+                }
+            }
+            done_tx.send(N_WORKERS);  // work consumer's ID is one greater than any producer's
+        });
+    }
+    for _ in 0..(N_WORKERS + 1) {
+        let wid = done_rx.recv();
+        println!("worker {:?} finished", wid);
     }
 }
