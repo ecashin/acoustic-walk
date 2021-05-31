26e67e21435fb8836a4359ead8423094c3d02e54
Ed Cashin <ed.cashin@acm.org>
2021-05-31 11:35:52 -0400

    Walk over dirs from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use chan;
-use std::{env,thread};
+use std::{env,path,thread};
 use walkdir::WalkDir;
 
 const N_WORKERS: u32 = 10;
 
-fn do_work(worker_id: u32, rdirs: chan::Receiver<String>, done: chan::Sender<()>) {
+fn do_work(worker_id: u32, rdirs: chan::Receiver<path::PathBuf>, done: chan::Sender<()>) {
     for dir in rdirs {
-        println!("worker:{} dir:{}", worker_id, dir);
+        println!("worker:{} dir:{:?}", worker_id, dir);
     }
     done.send(());
 }
@@ -16,6 +16,7 @@ fn main() {
 
     let (sdone, rdone) = chan::sync(0);
     {
+        // At the end of this scope, we're done sending directories.
         let (sdirs, rdirs) = chan::sync(0);
         for w in 0..N_WORKERS {
             let sdone = sdone.clone();
@@ -25,7 +26,10 @@ fn main() {
             });
         }
         for d in dirs {
-            sdirs.send(d);
+            for entry in WalkDir::new(d).into_iter().filter_map(|e| e.ok()) {
+                let p = path::PathBuf::from(entry.path());
+                sdirs.send(p);
+            }
         }
     }
     for _ in 0..N_WORKERS {
