06ed0552044c178220a5f42d5b0425aa3dc40278
Ed Cashin <ed.cashin@acm.org>
2021-05-31 14:06:38 -0400

    Add thread for consumer of work

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use chan;
 use std::{env,path,thread};
 use walkdir::WalkDir;
 
-const N_WORKERS: u32 = 10;
+const N_PRODUCERS: u32 = 10;
 
 fn do_work(worker_id: u32,
     paths_rx: chan::Receiver<path::PathBuf>,
-    wdescs_tx: chan::Sender<Option<WavDesc>>) {
+    wdescs_tx: chan::Sender<Option<WavDesc>>,
+    done_tx: chan::Sender<u32>) {
     for path in paths_rx {
         if path.is_file() {
             if let Some(ext) = path.extension() {
@@ -22,6 +23,7 @@ fn do_work(worker_id: u32,
         }
     }
     wdescs_tx.send(None);
+    done_tx.send(worker_id);
 }
 
 struct WavDesc {
@@ -30,18 +32,47 @@ struct WavDesc {
     // data: wav::BitDepth
 }
 
+fn consume(n_producers: u32, wdescs_rx: chan::Receiver<Option<WavDesc>>)
+{
+    let mut n = n_producers;
+    while n > 0 {
+        if let Some(wdesc_opt) = wdescs_rx.recv() {
+            if let Some(wdesc) = wdesc_opt {
+                println!("consumer received {:?} with {} producers remaining", wdesc.path, n);
+            } else {
+                println!("consumer received None");
+                n -= 1;
+            }
+        } else {
+            panic!("producers don't close the work channel");
+        }
+    }
+}
+
 fn main() {
     let dirs: Vec<String> = env::args().skip(1).collect();
 
+    let (done_tx, done_rx) = chan::sync(0);  // worker completion channel
     let (wdescs_tx, wdescs_rx) = chan::sync(0);  // wav description channel
     {
-        // At the end of this scope, we're done sending directories.
+        // Work consumer thread takes ownership of wdescs_rx.
+        let wdescs_rx = wdescs_rx;
+        let done_tx = done_tx.clone();
+        thread::spawn(move || {
+            consume(N_PRODUCERS, wdescs_rx);
+            done_tx.send(N_PRODUCERS);  // consumer ID is one greater than max producer ID
+        });
+    }
+
+    {
+        // At the end of this scope, dirs_tx dropped - we're done sending directories.
         let (dirs_tx, dirs_rx) = chan::sync(0);
-        for w in 0..N_WORKERS {
+        for w in 0..N_PRODUCERS {
             let dirs_rx = dirs_rx.clone();
+            let done_tx = done_tx.clone();
             let wdescs_tx = wdescs_tx.clone();
             thread::spawn(move || {
-                do_work(w, dirs_rx, wdescs_tx);
+                do_work(w, dirs_rx, wdescs_tx, done_tx);
             });
         }
         for d in dirs {
@@ -51,18 +82,11 @@ fn main() {
             }
         }
     }
-    // Work consumer thread takes ownership of wdescs_rx.
-    let mut n_producers = N_WORKERS;
-    while n_producers > 0 {
-        if let Some(wdesc_opt) = wdescs_rx.recv() {
-            if let Some(wdesc) = wdesc_opt {
-                println!("consumer received {:?} with {} producers remaining", wdesc.path, n_producers);
-            } else {
-                println!("consumer received None");
-                n_producers -= 1;
-            }
-        } else {
-            panic!("producers don't close the work channel");
+    let mut n_workers = N_PRODUCERS + 1;
+    while n_workers > 0 {
+        if let Some(worker_id) = done_rx.recv() {
+            println!("worker {} finished", worker_id);
+            n_workers -= 1;
         }
     }
 }
