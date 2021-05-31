602249515d3ecfa4d748ae9bb718a0093adf4a47
Ed Cashin <ed.cashin@acm.org>
2021-05-31 10:53:21 -0400

    Use Golang-like channel in prep for parallel file-tree walk work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use chan;
+use std::{env,thread};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let rx = {
+        let (tx, rx) = chan::sync(0);
+        for a in args.iter().skip(1) {
+            let tx = tx.clone();
+            let arg = a.clone();
+            thread::spawn(move || {
+                tx.send(arg);
+            });
+        }
+        rx
+    };
+    let wg = chan::WaitGroup::new();
+    for i in 0..args.len() {
+        println!("receiver {}", i);
+        wg.add(1);
+        let wg = wg.clone();
+        let rx = rx.clone();
+        thread::spawn(move || {
+            println!("{}:{:?}", i, rx.recv());
+            wg.done();
+        });
+    }
+    wg.wait();
 }
