f78634a23438d831791a5ec33c6ba12851a9d5a2
Ed Cashin <ed.cashin@acm.org>
2021-06-05 10:15:48 -0400

    Implement grain maker

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ const WAV_MAX_TTL: u32 = 10;
 
 struct Grain {
     start: u32,
-    pos: u32, // current position in grain
     len: u32,
     max_len: u32,
 }
@@ -29,7 +28,6 @@ impl Grain {
         let len = GRAIN_MS * sr_ms;
         Grain {
             start: 0,
-            pos: 0,
             max_len: len,
             len: len,
         }
@@ -41,17 +39,17 @@ impl Grain {
         let g_extra = g_right * g_right_fraction;
         let g_size = self.len as f32 * (MIN_GRAIN_SIZE_FRACTION + g_extra);
         self.len = g_size as u32;
-        self.start = rand_distr::Uniform::from(0..n - self.len).sample(&mut rng);
-        self.pos = 0;
+        let rounding_error = 1;  // one-sample safety margin
+        self.start = rand_distr::Uniform::from(0..n - rounding_error - self.len).sample(&mut rng);
     }
     // https://en.wikipedia.org/wiki/Window_function#Tukey_window
-    fn amplitude(&self, alpha: Option<f32>) -> f32 {
+    fn amplitude(&self, pos: usize, alpha: Option<f32>) -> f32 {
         let alpha = match alpha {
             Some(a) => a,
             None => DEFAULT_TUKEY_WINDOW_ALPHA,
         };
 
-        let n = self.pos as f32;
+        let n = pos as f32;
         let len = self.len as f32;
         let n = if n >= (len / 2.0) { len - n } else { n };
 
@@ -260,44 +258,58 @@ fn grain_samples(grain_ms: usize, sr: u32) -> usize {
 fn make_grains(
     grain_maker_id: u32,
     wavs: &Vec<WavDesc>,
-    grains_tx: chan::Sender<f32>,
+    grains_tx: chan::Sender<Vec<f32>>,
     sink_sr: usize,
 ) {
+    let mut g = Grain::new(sink_sr as u32);
     let wavs = wavs.clone();
-    let g = Grain::new(sink_sr as u32);
     thread::spawn(move || {
+        println!("grain maker {} starting", grain_maker_id);
         let mut rng = rand::thread_rng();
+        let mut send_buf: Vec<f32> = Vec::new();
         loop {
             let which = select_wavs(&wavs, 1)
                 .unwrap()
                 .iter()
                 .take(1)
+                .map(|e| *e)
                 .last()
                 .unwrap();
-            let wav = &wavs[*which];
-            let r = hound::WavReader::open(wav.path).ok().unwrap();
+            let wav = &wavs[which];
+            let mut r = hound::WavReader::open(wav.path.clone()).ok().unwrap();
             let src_sr = r.spec().sample_rate;
-            let src_per_sink = src_sr as f32 * sink_sr as f32;
             let ttl = rand_distr::Uniform::from(1..WAV_MAX_TTL).sample(&mut rng);
             for _ in 0..ttl {
                 g.land(wav.n_samples);
-                let n_read = (g.len as f32 * src_per_sink).ceil() as usize;
-                r.seek(g.start);
+                r.seek(g.start).ok();
                 let src_samples: Vec<f32> = r
                     .samples()
-                    .take(n_read)
+                    .take(g.len as usize)
                     .map(|e: Result<i16, hound::Error>| e.ok().unwrap() as f32 / i16::MAX as f32)
+                    .enumerate()
+                    .map(|(i, s)| s * g.amplitude(i, None))
                     .collect();
-                let sink_samples = convert(
+                let mut sink_samples = convert(
                     src_sr,
                     sink_sr as u32,
                     2,
                     ConverterType::SincBestQuality,
                     &src_samples[..],
-                );
-                // apply grain envelope
-                // push to vec
-                // split and send data if enough for buf
+                ).expect("converting sample rate");
+                send_buf.append(&mut sink_samples);
+                if send_buf.len() >= GRAIN_BUF_N_SAMPLES {
+                    let send_part: Vec<f32> =
+                        send_buf.iter()
+                        .take(GRAIN_BUF_N_SAMPLES)
+                        .map(|e| *e)
+                        .collect();
+                    let new_len = send_buf.len() - GRAIN_BUF_N_SAMPLES;
+                    for (i, j) in (0..new_len).enumerate() {
+                        send_buf[i] = send_buf[j];
+                    }
+                    send_buf.truncate(new_len);
+                    grains_tx.send(send_part);
+                }
             }
         }
     });
@@ -308,7 +320,7 @@ fn generate_samples(
     sink_sr: usize,
     wavs: Vec<WavDesc>,
 ) -> u32 {
-    let mut grains_rxs: Vec<chan::Receiver<f32>> = Vec::new();
+    let mut grains_rxs: Vec<chan::Receiver<Vec<f32>>> = Vec::new();
     for i in 0..N_GRAINS {
         let (grains_tx, grains_rx) = chan::sync(2);
         make_grains(i, &wavs, grains_tx, sink_sr);
@@ -320,7 +332,7 @@ fn generate_samples(
         let mut r = hound::WavReader::open(&wavs[0].path).expect("opening WAV file");
         let n_channels = r.spec().channels; // TODO: Use for each WAV
         let src_sr = r.spec().sample_rate;
-        let n_src_grain = grain_samples(GRAIN_MS, src_sr);
+        let n_src_grain = grain_samples(GRAIN_MS as usize, src_sr);
         let converter1 =
             Samplerate::new(ConverterType::SincBestQuality, src_sr, sink_sr as u32, 1).unwrap();
         let converter2 =
