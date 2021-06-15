b089b64dfdacea541ab0a9f6fc4b2c23bfd585ee
Ed Cashin <ed.cashin@acm.org>
2021-06-15 19:58:33 -0400

    Use first stereo config

--- src/cpal.rs
+++ src/cpal.rs
@@ -3,9 +3,11 @@ use cpal::traits::{DeviceTrait, HostTrait};
 pub fn cpal_demo() {
     let host = cpal::default_host();
     let device = host.default_output_device().expect("no output device available");
-    let mut supported_configs_range = device.supported_output_configs()
+    let supported_configs_range = device.supported_output_configs()
         .expect("error while querying configs");
-    let supported_config = supported_configs_range.next()
+    let supported_config = supported_configs_range
+        .filter(|c| c.channels() == 2)
+        .next()
         .expect("no supported config?!")
         .with_max_sample_rate();
     println!("{:?}", supported_config);
